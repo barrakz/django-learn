0319e328fe7be1ef494b5456895e0de446607946
barrakz <b.rakuzy@gmail.com>
2022-12-07 20:56:09 +0100

    first commit

---
+++ viewer/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db.models import (
+  DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
+  Model, TextField
+)
+
+
+class Genre(Model):
+    name = CharField(max_length=128)
+
+
+class Movie(Model):
+    title = CharField(max_length=128)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+    rating = IntegerField()
+    released = DateField()
+    description = TextField()
+    created = DateTimeField(auto_now_add=True)
+
+
+
